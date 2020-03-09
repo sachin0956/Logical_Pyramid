efc8e489db2150b097ff6080b02575649d3f1dc0
Sachin <Sachin@DESKTOP-21NNUUV>
2020-03-09 10:55:53 +0530

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+public static void main(String[] args) {
+		
+		for(int i=1;i<=4;i++) {
+			
+			for(int j=4;j>i;j--) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=(i);k++) {
+				System.out.print(" 1");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+			}
+}
